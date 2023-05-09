ced74c1a518ed8dd87ff5ff7c5ac634445f0a7d6
Vyshnav67 <vyshnavanchery2015@gmail.com>
2023-05-09 10:05:01 +0530

    first commit

---
+++ assets/js/test.js
@@ -0,0 +1,21 @@
+function img1(){
+    document.getElementById('changing').src="/basic_hero_ui@2x.webp";
+  }
+  function img2(){
+    document.getElementById('changing').src="/sui-dhaaga---release-in-china.jpg";
+  }
+  function img3(){
+    document.getElementById('changing').src="/CollabBrowse_2@2x.webp";
+  }
+  function img4(){
+    document.getElementById('changing').src="/collaboration-diagram.svg";
+  }
+  function img5(){
+    document.getElementById('changing').src="/JTBD_UI_7@2x.webp";
+  }
+  function img6(){
+    document.getElementById('changing').src="/pro_plank_01_ui@2x.png";
+  }
+  function img7(){
+    document.getElementById('changing').src="/pro_plank_02_ui@2x.png";
+  }
